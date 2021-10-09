45e5a23fdf6b7efaff32f77e59d071b2c878e2e5
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-10-09 15:56:21 +0100

    Update squares2.py

--- squares2.py
+++ squares2.py
@@ -1,35 +1,29 @@
 """
 Makes NFT Style image
 ready for another image to be merged with it
+and adds text
 """
-
-import os
 import random
 import svgwrite
 import svgutils.transform as st
-# from pathlib import Path
-# from svglib.svglib import svg2rlg
-# from reportlab.graphics import renderPM
+from pathlib import Path
 
-# Constants
-PATH = 'output'
-IMG_H = 400
-IMG_W = 400
-SRC_IMG = "pi_400.svg"
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
 
 
 class Dwg:
 
-    """create class for a clear svg of rectangles"""
+    """create class for rectangles"""
 
-    def __init__(self, sqheight=10, sqwidth=10, fill="rgb(135,125,24)"):
+    def __init__(self, height=10, width=10, fill="rgb(255,255,44)"):
 
-        self.width = sqheight
-        self.height = sqwidth
+        self.width = height
+        self.height = width
 
         self.dwg = svgwrite.Drawing(
             "test.svg",
-            size=(IMG_H, IMG_W),
+            size=(400, 400),
             profile="tiny",
             stroke_width=0.5,
             stroke="black",
@@ -37,11 +31,11 @@ class Dwg:
             fill=fill,
         )
 
-    def draw_rectangles_tl(self):
+    def draw_rectangle(self):
 
         for x in range(0, self.width):
             for y in range(0, self.height):
-                pos_mod_x = (random.random() - 0.5) * (x + (x * self.width))
+                pos_mod_x = (random.random() - 0.5) * (x + (y * self.width))
                 pos_mod_y = (random.random() - 0.5) * (x + (y * self.height))
                 self.dwg.add(
                     self.dwg.rect(
@@ -50,63 +44,59 @@ class Dwg:
                     )
                 )
 
-    def draw_rectangles_br(self):
+    def draw_text(self):
 
-        for x in range(0, self.width):
-            for y in range(0, self.height):
-                pos_mod_x = (random.random() - 0.5) * (x + (x * self.width))
-                pos_mod_y = (random.random() - 0.5) * (x + (y * self.height))
-                self.dwg.add(
-                    self.dwg.rect(
-                        (IMG_W - x * 10 + pos_mod_x, IMG_H - y * 10 + pos_mod_y),
-                        (self.width, self.height)
-                    )
-                )
+        self.dwg.add(
+            self.dwg.text(
+                "Dr Pi",
+                insert=(55, 125),
+                stroke="none",
+                fill="#900",
+                font_size="90px",
+                font_weight="bold",
+                font_family="Arial",
+            )
+        )
 
     def save_as(self, filename):
         self.dwg.saveas(filename)
 
+
 # main
-
-
 if __name__ == "__main__":
 
-    os.chdir(PATH)
+    path = Path("source_images/pi_400.svg")
+    assert path.is_file(), "pi_400 missing"
 
     d1 = Dwg()
-    d1.draw_rectangles_tl()
+    d1.draw_rectangle()
     d1.save_as("first.svg")
 
-    d2 = Dwg(sqheight=15, fill="rgb(25,25,214)")
-    d2.draw_rectangles_br()
+    d2 = Dwg(height=15, fill="rgb(255,255,214)")
+    d2.draw_rectangle()
     d2.save_as("second.svg")
 
-    d3 = Dwg(sqwidth=15, fill="rgb(25,185,214)")
-    d3.draw_rectangle_tl()
+    d3 = Dwg(width=15, fill="rgb(255,85,214)")
+    d3.draw_rectangle()
+    d3.draw_text()  # Add ome text
     d3.save_as("third.svg")
 
-    # The main image
-    template = st.fromfile(SRC_IMG)
-
-    # The overlays
-    first_svg = st.fromfile("first.svg")
+    template = st.fromfile("source_images/pi_400.svg")
     second_svg = st.fromfile("second.svg")
     third_svg = st.fromfile("third.svg")
 
-    template.append(first_svg)
     template.append(second_svg)
     template.append(third_svg)
 
     # save merged file
     template.save("merged.svg")
 
-    '''convert svgs to png if required'''
+    # save source SVG files
+    drawing = svg2rlg("first.svg")
+    renderPM.drawToFile(drawing, "source_images/first.png", fmt="PNG")
 
-    # drawing = svg2rlg("first.svg")
-    # renderPM.drawToFile(drawing, "source_images/first.png", fmt="PNG")
+    drawing = svg2rlg("second.svg")
+    renderPM.drawToFile(drawing, "source_images/second.png", fmt="PNG")
 
-    # drawing = svg2rlg("second.svg")
-    # renderPM.drawToFile(drawing, "source_images/second.png", fmt="PNG")
-
-    # drawing = svg2rlg("third.svg")
-    # renderPM.drawToFile(drawing, "source_images/third.png", fmt="PNG")
+    drawing = svg2rlg("third.svg")
+    renderPM.drawToFile(drawing, "source_images/third.png", fmt="PNG")
